f902580a1f8960d37edae7f53d358048f68b0cf1
Jim Oxenford <joxenford@me.com>
2014-02-02 07:47:53 -0500

    removed edit button on table

--- TestMachine/AppDelegate.h
+++ TestMachine/AppDelegate.h
@@ -1,11 +1,3 @@
-//
-//  AppDelegate.h
-//  TestMachine
-//
-//  Created by James Oxenford on 1/25/14.
-//  Copyright (c) 2014 James Oxenford. All rights reserved.
-//
-
 #import <UIKit/UIKit.h>
 
 @interface AppDelegate : UIResponder <UIApplicationDelegate>
