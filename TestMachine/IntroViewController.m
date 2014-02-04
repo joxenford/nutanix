6203884886b9cfc104db12440bdce6a621cf7187
Jim Oxenford <joxenford@me.com>
2014-02-03 20:06:04 -0500

    embed video working

---
+++ TestMachine/IntroViewController.m
@@ -0,0 +1,21 @@
+#import "IntroViewController.h"
+#import "Themer.h"
+
+@interface IntroViewController ()
+
+@property (weak, nonatomic) IBOutlet UILabel* nutanixIsASoftwareLabel;
+@property (weak, nonatomic) IBOutlet UILabel* nutanixVirtualComputingLabel;
+@property (weak, nonatomic) IBOutlet UILabel* gladYouAskedLabel;
+
+@end
+
+@implementation IntroViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+
+@end
