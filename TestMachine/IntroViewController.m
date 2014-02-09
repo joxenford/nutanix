287828e51bf5040ea33b0b564e0346ade221f011
Jim Oxenford <joxenford@me.com>
2014-02-08 20:12:07 -0500

    added some animations

--- TestMachine/IntroViewController.m
+++ TestMachine/IntroViewController.m
@@ -6,6 +6,7 @@
 @property (weak, nonatomic) IBOutlet UILabel* nutanixIsASoftwareLabel;
 @property (weak, nonatomic) IBOutlet UILabel* nutanixVirtualComputingLabel;
 @property (weak, nonatomic) IBOutlet UILabel* gladYouAskedLabel;
+@property (weak, nonatomic) IBOutlet UIButton* nextButton;
 
 @end
 
@@ -14,8 +15,19 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view.
+    [self fadeIn:_nutanixIsASoftwareLabel delay:1];
+    [self fadeIn:_nutanixVirtualComputingLabel delay:5];
+    [self fadeIn:_gladYouAskedLabel delay:10];
+    [self fadeIn:_nextButton delay:12];
 }
 
+- (void) fadeIn:(UIView*) label delay:(float) delay;
+{
+    label.alpha = 0;
+    
+    [UIView animateWithDuration:1.0 delay:delay options:UIViewAnimationOptionCurveEaseIn
+                     animations:^{ label.alpha = 1;}
+                     completion:nil];
+}
 
 @end
